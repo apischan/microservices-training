0f75328d681dc6dcbf44f507ba2650e436e646ac
apischan <andrew.pischanski@gmail.com>
2016-08-04 14:49:34 +0300

    fixed jar names

--- microservices-bat/eureka-server/run-tertiary.sh
+++ microservices-bat/eureka-server/run-tertiary.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -jar -Dspring.profiles.active=tertiary ../../eureka-server/target/lab4-eureka-server-0.0.1-SNAPSHOT.jar
+java -jar -Dspring.profiles.active=tertiary ../../eureka-server/target/eureka-server-0.0.1-SNAPSHOT.jar
