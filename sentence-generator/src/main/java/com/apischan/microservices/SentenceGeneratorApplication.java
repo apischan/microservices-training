79b7e97660f9ec2d461f426eaf363c77bb2fbe52
apischan <andrew.pischanski@gmail.com>
2016-08-09 02:27:04 +0300

    added hystrix dashboard

--- sentence-generator/src/main/java/com/apischan/microservices/SentenceGeneratorApplication.java
+++ sentence-generator/src/main/java/com/apischan/microservices/SentenceGeneratorApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
 public class SentenceGeneratorApplication {
 
