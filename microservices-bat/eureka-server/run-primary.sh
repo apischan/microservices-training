f9e9d0942c31c72021a2f2fa06367ac40d661fd7
apischan <andrew.pischanski@gmail.com>
2016-08-09 20:23:16 +0300

    added min value of heap size

--- microservices-bat/eureka-server/run-primary.sh
+++ microservices-bat/eureka-server/run-primary.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -jar -Dspring.profiles.active=primary ../../eureka-server/target/eureka-server-0.0.1-SNAPSHOT.jar
+java -jar -Xmx128m -Dspring.profiles.active=primary ../../eureka-server/target/eureka-server-0.0.1-SNAPSHOT.jar
