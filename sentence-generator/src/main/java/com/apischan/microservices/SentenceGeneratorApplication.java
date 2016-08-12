b9797609b63a6502154aacdcb6d56d05a241e7c1
apischan <andrew.pischanski@gmail.com>
2016-08-12 12:51:31 +0300

    added Feign logging

--- sentence-generator/src/main/java/com/apischan/microservices/SentenceGeneratorApplication.java
+++ sentence-generator/src/main/java/com/apischan/microservices/SentenceGeneratorApplication.java
@@ -1,20 +1,30 @@
 package com.apischan.microservices;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
 @EnableHystrixDashboard
 @SpringBootApplication
+@Configuration
 public class SentenceGeneratorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SentenceGeneratorApplication.class, args);
     }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
 }
