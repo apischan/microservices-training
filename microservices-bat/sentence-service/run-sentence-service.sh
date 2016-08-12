3c11baa3a414b6933999e0060b8d84ba1ad51f06
apischan <andrew.pischanski@gmail.com>
2016-08-12 19:53:18 +0300

    increase min heap size

--- microservices-bat/sentence-service/run-sentence-service.sh
+++ microservices-bat/sentence-service/run-sentence-service.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -jar -Xmx64m ../../sentence-generator/target/sentence-generator-0.0.1-SNAPSHOT.jar
+java -jar -Xmx128m ../../sentence-generator/target/sentence-generator-0.0.1-SNAPSHOT.jar
